e138b088c9f43a87a7eedbcaf533ab0a2d455104
GKB <gkbm2013@gmail.com>
2022-08-19 14:48:27 +0800

    add rollup.js

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import { babel } from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import * as fs from "fs";
+
+const extensions = [".ts", ".js"];
+
+const preventTreeShakingPlugin = () => {
+    return {
+        name: "no-treeshaking",
+        resolveId(id, importer) {
+            if (!importer) {
+                // let's not treeshake entry points, as we're not exporting anything in App Scripts
+                return { id, moduleSideEffects: "no-treeshake" };
+            }
+            return null;
+        }
+    };
+};
+
+const plugins = [
+    preventTreeShakingPlugin(),
+    nodeResolve({
+        extensions,
+        mainFields: ["jsnext:main", "main"]
+    }),
+    babel({ extensions, babelHelpers: "runtime" }),
+];
+
+const createConfig = (filename) => ({
+    input: `src/${filename}`,
+    output: {
+        dir: "build",
+        format: "cjs",
+    },
+    plugins,
+});
+
+// Source: https://github.com/jaebradley/example-rollup-library/blob/master/rollup.config.js
+const configs = fs.readdirSync("./src").map((filename) => createConfig(filename));
+
+
+export default configs;
