7986fd53735e5f82f48b12194d731b7b34eb5261
GKB <gkbm2013@gmail.com>
2022-08-19 19:57:32 +0800

    implement client for curseforge API

---
+++ src/curseforge.ts
@@ -0,0 +1,99 @@
+import * as URI from "uri-js";
+
+class CurseForgeApi {
+    apiKey: string;
+    endpointHost: string;
+
+    constructor(apiKey: string, endpointHost = "api.curseforge.com") {
+        this.apiKey = apiKey;
+        this.endpointHost = endpointHost;
+    }
+
+    fetch(requestPath: string, query?: string): GoogleAppsScript.URL_Fetch.HTTPResponse {
+        const comonents: URI.URIComponents = { scheme: "https", host: this.endpointHost, path: requestPath };
+        if (query) {
+            comonents.query = query;
+        }
+        const uri = URI.serialize(comonents);
+        const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+            "method": "get",
+            "headers": {
+                "x-api-key": this.apiKey,
+                "Accept": "application/json"
+            }
+        };
+        const response = UrlFetchApp.fetch(uri, options);
+        return response;
+    }
+
+    getGameVersions(gameId: number): GetVersionsResponse {
+        const requestPath = `/v1/games/${gameId}/versions`;
+        const response = this.fetch(requestPath);
+        if (response.getResponseCode() != 200) {
+            throw new Error(`Server error: ${response.getResponseCode()}`);
+        }
+        const verResp: GetVersionsResponse = JSON.parse(response.getContentText());
+        return verResp;
+    }
+
+    getMinecraftVersions(): GetVersionsResponse {
+        return this.getGameVersions(432);
+    }
+
+    getModFiles(modId: number): File[] {
+        const pageSize = 50;
+
+        let currentIndex = 0;
+        let remaining = -1;
+
+        const files: File[] = [];
+
+        while (remaining != 0) {
+            const requestPath = `/v1/mods/${modId}/files`;
+            const query = `index=${currentIndex}&pageSize=${pageSize}`;
+            const response = this.fetch(requestPath, query);
+            if (response.getResponseCode() != 200) {
+                throw new Error(`Server error: ${response.getResponseCode()}`);
+            }
+            const filesResp: GetModFilesResponse = JSON.parse(response.getContentText());
+            const pagination = filesResp.pagination;
+            remaining = pagination.totalCount - (pagination.index + pagination.resultCount);
+            currentIndex = pagination.index + pagination.resultCount;
+            files.push(...filesResp.data);
+        }
+
+        return files;
+    }
+}
+
+function testGetMcVersions() {
+    let scriptProperties = null;
+    try {
+        scriptProperties = PropertiesService.getScriptProperties();
+    } catch (e) {
+        Logger.log(e);
+        return;
+    }
+    const apiKey = scriptProperties.getProperty("CF_API_KEY");
+    if (apiKey == null) {
+        throw Error("Please set property `CF_API_KEY`");
+    }
+    const api = new CurseForgeApi(apiKey);
+    return api.getMinecraftVersions();
+}
+
+function testGetModFiles() {
+    let scriptProperties = null;
+    try {
+        scriptProperties = PropertiesService.getScriptProperties();
+    } catch (e) {
+        Logger.log(e);
+        return;
+    }
+    const apiKey = scriptProperties.getProperty("CF_API_KEY");
+    if (apiKey == null) {
+        throw Error("Please set property `CF_API_KEY`");
+    }
+    const api = new CurseForgeApi(apiKey);
+    return api.getModFiles(229503);
+}
